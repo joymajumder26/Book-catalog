ab7630a4cf881ba0d02fcc78655805281e017aca
joymajumder26 <joymajumder34@gmail.com>
2023-07-25 21:19:28 +0600

    update

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://book-catalog-server-gray.vercel.app/' }),
   tagTypes:['comments',"create-book","delete-book",'update-book','books'],
   endpoints: () => ({})
 
